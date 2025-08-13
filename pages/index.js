dfcabf5f1db43f55bd1f65b0f086b1e152476d45
BAYRESTORE <prbayuu@gmail.com>
2025-08-13 23:38:27 +0700

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,18 @@
+import Navbar from '../components/Navbar'
+import PostCard from '../components/PostCard'
+import Sidebar from '../components/Sidebar'
+
 export default function Home() {
   return (
-    <main style={{ textAlign: "center", marginTop: "50px" }}>
-      <h1>Halo, ini website barumu 🚀</h1>
-      <p>Website ini dibuat dengan Next.js dan siap di-deploy ke Vercel.</p>
-    </main>
-  );
+    <div className="bg-gray-100 min-h-screen">
+      <Navbar />
+      <main className="flex justify-center max-w-5xl mx-auto mt-4">
+        <div className="w-full lg:w-2/3 px-4">
+          <PostCard username="andi" image="/logo.png" caption="Posting pertama!" />
+          <PostCard username="budi" image="/logo.png" caption="Halo semua!" />
+        </div>
+        <Sidebar />
+      </main>
+    </div>
+  )
 }
