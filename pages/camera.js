e601ce66ac7610d79fd940fcba43b32af7e4c339
BAYRESTORE <prbayuu@gmail.com>
2025-08-13 23:53:40 +0700

    Update camera.js

--- pages/camera.js
+++ pages/camera.js
@@ -11,4 +11,17 @@ export default function CameraPage() {
       </main>
     </div>
   )
+}import Navbar from '../components/Navbar'
+import CameraComponent from '../components/CameraComponent'
+
+export default function CameraPage() {
+  return (
+    <div className="bg-gray-100 min-h-screen">
+      <Navbar />
+      <main className="max-w-5xl mx-auto p-4">
+        <h1 className="text-xl font-bold mb-4">Kamera</h1>
+        <CameraComponent />
+      </main>
+    </div>
+  )
 }
