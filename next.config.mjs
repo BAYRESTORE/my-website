ee16a08b1d14e9b10954b1ff0b02b179c1885010
BAYRESTORE <prbayuu@gmail.com>
2025-08-14 00:32:36 +0700

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,9 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  swcMinify: false, // Matikan minify biar nggak pakai lightningcss
+  swcMinify: false, // Matikan minify
   experimental: {
-    optimizeCss: false, // Matikan CSS optimizer yang pakai lightningcss
+    optimizeCss: false, // Matikan CSS optimizer lightningcss
   },
 };
 
