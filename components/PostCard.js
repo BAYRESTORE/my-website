2564e09c7302dbb07d1196755fa223074980f1d8
BAYRESTORE <prbayuu@gmail.com>
2025-08-13 23:41:12 +0700

    PostCard.js

---
+++ components/PostCard.js
@@ -0,0 +1,12 @@
+export default function PostCard({ username, image, caption }) {
+  return (
+    <div className="bg-white border rounded-md my-4">
+      <div className="px-4 py-2 font-bold">{username}</div>
+      <img src={image} alt="Post" className="w-full" />
+      <div className="px-4 py-2">
+        <p className="font-semibold">{username}</p>
+        <p>{caption}</p>
+      </div>
+    </div>
+  )
+}
