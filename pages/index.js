79e1050ae0ae983f9d2c0bd1c483aa6796afa364
BAYRESTORE <prbayuu@gmail.com>
2025-08-13 23:33:47 +0700

    index.js

---
+++ pages/index.js
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <main style={{ textAlign: "center", marginTop: "50px" }}>
+      <h1>Halo, ini website barumu 🚀</h1>
+      <p>Website ini dibuat dengan Next.js dan siap di-deploy ke Vercel.</p>
+    </main>
+  );
+}
