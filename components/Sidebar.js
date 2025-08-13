25e64daa5748d30a2eb746a5f1fc0f7b8f879a60
BAYRESTORE <prbayuu@gmail.com>
2025-08-13 23:40:35 +0700

    Sidebar.js

---
+++ components/Sidebar.js
@@ -0,0 +1,14 @@
+export default function Sidebar() {
+  return (
+    <aside className="hidden lg:block w-1/3 p-4">
+      <div className="bg-white border rounded-md p-4">
+        <h2 className="font-bold">Saran untukmu</h2>
+        <ul className="mt-2 space-y-2">
+          <li>🔥 User 1</li>
+          <li>🌟 User 2</li>
+          <li>🎯 User 3</li>
+        </ul>
+      </div>
+    </aside>
+  )
+}
